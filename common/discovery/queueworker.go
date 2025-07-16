30796427dbd9dc323ce82857bf25a0593cb62e17
codingdeng <2804179081@qq.com>
2025-07-16 16:02:51 +0800

    etcd && kafka consumer

---
+++ common/discovery/queueworker.go
@@ -0,0 +1,78 @@
+package discovery
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/zeromicro/go-queue/kq"
+	"github.com/zeromicro/go-zero/core/logx"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+type QueueWorker struct {
+	key    string
+	kqConf kq.KqConf
+	client *clientv3.Client
+}
+
+func NewQueueWorker(key string, endpoints []string, kqConf kq.KqConf) *QueueWorker {
+	cfg := clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: time.Second * 3,
+	}
+	etcdClient, err := clientv3.New(cfg)
+	if err != nil {
+		panic(err)
+	}
+	return &QueueWorker{
+		key:    key,
+		client: etcdClient,
+		kqConf: kqConf,
+	}
+}
+
+func (q *QueueWorker) HeartBeat() {
+	value, err := json.Marshal(q.kqConf)
+	if err != nil {
+		panic(err)
+	}
+	q.register(string(value), context.Background())
+}
+func (q *QueueWorker) register(value string, ctx context.Context) {
+	leaseGrantResp, err := q.client.Grant(ctx, 45)
+	if err != nil {
+		panic(err)
+	}
+	leaseId := leaseGrantResp.ID
+	logx.Infof("leaseID: %x", leaseId)
+	kv := clientv3.NewKV(q.client)
+	putResp, err := kv.Put(ctx, q.key, value, clientv3.WithLease(leaseId))
+	if err != nil {
+		panic(err)
+	}
+
+	keepRespChan, err := q.client.KeepAlive(context.TODO(), leaseId)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		//nolint:gocritic
+		for {
+			select {
+			case keepResp, ok := <-keepRespChan:
+				if !ok {
+					logx.Infof("租约已经失效:%x", leaseId)
+					q.register(value, ctx)
+					return
+				} else {
+					logx.Infof("收到自动续租应答:%x", keepResp.ID)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	logx.Info("写入成功:", putResp.Header.Revision)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.0
 
 require (
 	github.com/spaolacci/murmur3 v1.1.0
+	github.com/zeromicro/go-queue v1.2.2
 	github.com/zeromicro/go-zero v1.8.5
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.36.5
@@ -46,11 +47,13 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/openzipkin/zipkin-go v0.4.3 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/prometheus/client_golang v1.21.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/redis/go-redis/v9 v9.11.0 // indirect
+	github.com/segmentio/kafka-go v0.4.47 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.15 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.15 // indirect
 	go.etcd.io/etcd/client/v3 v3.5.15 // indirect
